d5fdf4fdabb8bda99442f3c79153bd73a6023c33
SpliiT <spliitdev@gmail.com>
2024-05-17 14:22:00 +0200

    rename script

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,6 @@ RUN apt-get update && apt-get install -y wget
 
 ENV GEODE_LICENSE_LOCATION=/server/geode.lic
 
-CMD "run_server"
+CMD "geodeapp_server"
 
 EXPOSE 5000
--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from src.geodeapp_back.app import app, run_server
+from src.geodeapp_back.app import app
 
 
 @pytest.fixture
@@ -9,6 +9,5 @@ def client():
     app.config["SERVER_NAME"] = "TEST"
     app.config["DATA_FOLDER"] = "./data/"
     client = app.test_client()
-    # client = run_server()
     client.headers = {"Content-type": "application/json", "Accept": "application/json"}
     yield client
