e5853968df1bf5df6f31c4f0c09d78636c953d00
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-08-26 10:51:27 +0200

    feat(app): desktop mode and dynamic origins

--- src/geodeapp_back/config.py
+++ src/geodeapp_back/config.py
@@ -1,22 +1,24 @@
-""" Flask configuration """
-
+# Standard library imports
 import os
+import time
 
+# Third party imports
+# Local application imports
 
 class Config(object):
     FLASK_DEBUG = os.environ.get("FLASK_DEBUG", default=False)
-    ID = os.environ.get("ID", default=None)
     DEFAULT_PORT = "5000"
     CORS_HEADERS = "Content-Type"
     UPLOAD_FOLDER = "./uploads"
-    WORKFLOWS_DATA_FOLDER = "./data_workflows/"
-    LOCK_FOLDER = "./lock/"
-    TIME_FOLDER = "./time/"
+    DESKTOP_APP = False
+    REQUEST_COUNTER = 0
+    LAST_REQUEST_TIME=time.time()
+    LAST_PING_TIME=time.time()
 
 
 class ProdConfig(Config):
     SSL = None
-    ORIGINS = ["https://geode-solutions.com", "https://next.geode-solutions.com"]
+    ORIGINS = ""
     MINUTES_BEFORE_TIMEOUT = "5"
     SECONDS_BETWEEN_SHUTDOWNS = "150"
     DEFAULT_DATA_FOLDER_PATH = "/data/"
@@ -24,7 +26,7 @@ class ProdConfig(Config):
 
 class DevConfig(Config):
     SSL = None
-    ORIGINS = "http://localhost:3000"
+    ORIGINS = "*"
     MINUTES_BEFORE_TIMEOUT = "1000"
     SECONDS_BETWEEN_SHUTDOWNS = "60"
     DEFAULT_DATA_FOLDER_PATH = "./data/"
