c1cca4f2930736b9e0fa6ea7fd4507b609576c9b
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-01-13 17:26:18 +0100

    refactor routes & schemas

--- src/tests/test_routes.py
+++ src/tests/test_routes.py
@@ -9,12 +9,22 @@ def test_root(client):
     response = client.post(route)
     assert response.status_code == 200
 
-def test_versions(client):
-    route = f"/vease/versions"
+
+def test_packages_versions(client):
+    route = f"/vease/packages_versions"
     response = client.get(route)
     assert response.status_code == 200
-    versions = response.json["versions"]
-    print(type(versions), versions, flush=True)
-    assert type(versions) is list
-    for version in versions:
+    packages_versions = response.json["packages_versions"]
+    print(type(packages_versions), packages_versions, flush=True)
+    assert type(packages_versions) is list
+    for version in packages_versions:
         assert type(version) is dict
+
+
+def test_microservice_version(client):
+    route = f"/vease/microservice_version"
+    response = client.get(route)
+    assert response.status_code == 200
+    microservice_version = response.json["microservice_version"]
+    print(type(microservice_version), microservice_version, flush=True)
+    assert type(microservice_version) is str
--- src/vease_back/routes/blueprint_vease.py
+++ src/vease_back/routes/blueprint_vease.py
@@ -9,20 +9,22 @@ from opengeodeweb_back import utils_functions
 
 schemas = os.path.join(os.path.dirname(__file__), "schemas")
 
-with open(os.path.join(schemas, "versions.json"), "r") as file:
-    vease_versions_json = json.load(file)
+with open(os.path.join(schemas, "packages_versions.json"), "r") as file:
+    packages_versions_json = json.load(file)
+
+with open(os.path.join(schemas, "microservice_version.json"), "r") as file:
+    microservice_version_json = json.load(file)
 
 
 routes = flask.Blueprint("vease_routes", __name__)
 flask_cors.CORS(routes)
 
 
-
 @routes.route(
-    vease_versions_json["route"], methods=vease_versions_json["methods"]
+    packages_versions_json["route"], methods=packages_versions_json["methods"]
 )
-def versions():
-    utils_functions.validate_request(flask.request, vease_versions_json)
+def packages_versions():
+    utils_functions.validate_request(flask.request, packages_versions_json)
     list_packages = [
         "OpenGeode-core",
         "OpenGeode-Geosciences",
@@ -39,5 +41,17 @@ def versions():
         "Geode-Viewables",
     ]
     return flask.make_response(
-        {"versions": utils_functions.versions(list_packages)}, 200
+        {"packages_versions": utils_functions.versions(list_packages)}, 200
+    )
+
+
+@routes.route(
+    microservice_version_json["route"], methods=microservice_version_json["methods"]
+)
+def microservice_version():
+    utils_functions.validate_request(flask.request, microservice_version_json)
+    list_packages = ["vease-back"]
+    return flask.make_response(
+        {"microservice_version": utils_functions.versions(list_packages)[0]["version"]},
+        200,
     )
