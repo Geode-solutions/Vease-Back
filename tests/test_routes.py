f644b4014badb3422be00e1b312c74445f7fe968
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-12-02 09:44:21 +0100

    remove json

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,6 +1,6 @@
 def test_allowed_files(client):
     route = f"/opengeodeweb_back/allowed_files"
-    response = client.post(route, json={})
+    response = client.post(route)
     assert response.status_code == 200
 
 
