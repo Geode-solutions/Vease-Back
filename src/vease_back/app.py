6ea0a03e194d79306208f0b5d154dc1418ad0405
JulienChampagnol <julien.champagnol@geode-solutions.com>
2026-01-07 16:19:05 +0100

    Feat(App): create_app, register_ogw_back_blueprints, run_server

--- src/vease_back/app.py
+++ src/vease_back/app.py
@@ -1,22 +1,20 @@
 # Standard library imports
 
 # Third party imports
-from opengeodeweb_back.app import app, run_server
+from opengeodeweb_back.app import create_app, run_server, register_ogw_back_blueprints
 
 # Local application imports
 import vease_back.routes.blueprint_vease as blueprint_vease
 
-
-app.register_blueprint(
-    blueprint_vease.routes,
-    url_prefix="/vease_back",
-    name="vease",
-)
-
-
 def run_vease_back() -> None:
-    run_server()
-
+    app = create_app(__name__)
+    register_ogw_back_blueprints(app)
+    app.register_blueprint(
+        blueprint_vease.routes,
+        url_prefix="/vease_back",
+        name="vease",
+    )
+    run_server(app)
 
 if __name__ == "__main__":
     run_vease_back()
